9b4845f7c8ecb7411c38afe8ccdeca0f34a89a8d
Jisan <murad305bp@gmail.com>
2023-06-02 12:38:40 +0600

    ss

---
+++ changdetecter.py
@@ -0,0 +1,92 @@
+import requests
+from bs4 import BeautifulSoup
+import difflib
+import os
+import tkinter as tk
+from tkinter import messagebox
+from tkinter.scrolledtext import ScrolledText
+
+def fetch_webpage(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    for script in soup(["script", "style"]):
+        script.decompose()
+    text = soup.get_text()
+    cleaned_text = " ".join(text.split())
+    return cleaned_text
+
+def save_content(content, filename):
+    with open(f'{filename}.txt', 'w') as f:
+        f.write(content.replace('\n', ' '))
+
+def load_content(filename):
+    if os.path.isfile(f'{filename}.txt'):
+        with open(f'{filename}.txt') as f:
+            return f.read()
+    return None
+
+def has_content_changed(new_content, old_content):
+    return new_content != old_content
+
+def get_differences(new_content, old_content):
+    differ = difflib.Differ()
+    new_sentences = new_content.split(". ")
+    old_sentences = old_content.split(". ")
+    differences = list(differ.compare(old_sentences, new_sentences))
+    return '\n'.join(line for line in differences if line.startswith('- ') or line.startswith('+ '))
+
+def check_for_changes(url, filename):
+    new_content = fetch_webpage(url)
+    old_content = load_content(filename)
+    if old_content is not None:
+        if has_content_changed(new_content, old_content):
+            differences = get_differences(new_content, old_content)
+            save_content(new_content, filename)  # Save the content immediately
+            show_changed_content(new_content, differences)  # Display the changed content
+        else:
+            show_no_change_message()
+    else:
+        save_content(new_content, filename)  # Save the content for the first time
+        messagebox.showinfo("Initial Save", "Initial content has been saved.")
+
+def show_no_change_message():
+    window = tk.Toplevel()
+    window.title("Data Not Changed")
+
+    message_label = tk.Label(window, text="Data has not changed.")
+    message_label.pack()
+
+    window.mainloop()
+
+def show_changed_content(content, differences):
+    window = tk.Toplevel()
+    window.title("Changed Content")
+
+    text_widget = ScrolledText(window, height=30, width=100)
+    text_widget.insert(tk.END, content + "\n\nDifferences:\n" + differences)
+    text_widget.pack()
+
+    window.mainloop()
+
+def run_gui():
+    window = tk.Tk()
+    window.title("Webpage Monitor")
+    
+    url_label = tk.Label(window, text="URL:")
+    url_label.pack()
+    url_entry = tk.Entry(window)
+    url_entry.pack()
+    
+    filename_label = tk.Label(window, text="Filename (without extension):")
+    filename_label.pack()
+    filename_entry = tk.Entry(window)
+    filename_entry.pack()
+    
+    submit_button = tk.Button(window, text="Monitor Changes", command=lambda: check_for_changes(url_entry.get(), filename_entry.get()))
+    submit_button.pack()
+    
+    window.mainloop()
+
+if __name__ == "__main__":
+    run_gui()
+

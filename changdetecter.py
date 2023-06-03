2d20db0d279fe2e68c7ffd93bae0bef078c5b1d7
Jisan <murad305bp@gmail.com>
2023-06-04 00:09:52 +0600

    10update

--- changdetecter.py
+++ changdetecter.py
@@ -15,9 +15,14 @@ def fetch_webpage(url):
     cleaned_text = " ".join(text.split())
     return cleaned_text
 
+def overwrite_file_content(content, filename):
+    with open(f'{filename}.txt', 'w') as f:  # 'w' for overwriting
+        f.write(content.replace('', ''))
+
+
 def save_content(content, filename):
-    with open(f'{filename}.txt', 'w') as f:
-        f.write(content.replace('\n', ' '))
+    with open(f'{filename}.txt', 'a') as f:  # 'a' for appending instead of overwriting
+        f.write(content.replace('', ''))
 
 def load_content(filename):
     if os.path.isfile(f'{filename}.txt'):
@@ -26,7 +31,7 @@ def load_content(filename):
     return None
 
 def has_content_changed(new_content, old_content):
-    return new_content != old_content
+    return new_content not in old_content
 
 def get_differences(new_content, old_content):
     differ = difflib.Differ()
@@ -35,19 +40,32 @@ def get_differences(new_content, old_content):
     differences = list(differ.compare(old_sentences, new_sentences))
     return '\n'.join(line for line in differences if line.startswith('- ') or line.startswith('+ '))
 
-def check_for_changes(url, filename):
-    new_content = fetch_webpage(url)
+
+
+def check_for_changes(urls, filename):
     old_content = load_content(filename)
-    if old_content is not None:
-        if has_content_changed(new_content, old_content):
-            differences = get_differences(new_content, old_content)
-            save_content(new_content, filename)  # Save the content immediately
-            show_changed_content(new_content, differences)  # Display the changed content
+    all_new_content = ""  # Collect all new content to show and save later
+    initial_save = False  # Track if it's the first time saving content
+
+    for url in urls:
+        new_content = fetch_webpage(url)
+        all_new_content += new_content # Collect the new content for each URL
+
+    if old_content is None:
+        initial_save = True
+    else:
+        differences = get_differences(all_new_content, old_content)
+        if differences:
+            show_changed_content(all_new_content, differences, urls, filename)  # Display the changed content
         else:
             show_no_change_message()
-    else:
-        save_content(new_content, filename)  # Save the content for the first time
+
+    if initial_save:
+        save_content(all_new_content, filename)  # Always save the new content at first
         messagebox.showinfo("Initial Save", "Initial content has been saved.")
+
+
+
 
 def show_no_change_message():
     window = tk.Toplevel()
@@ -58,7 +76,8 @@ def show_no_change_message():
 
     window.mainloop()
 
-def show_changed_content(content, differences):
+
+def show_changed_content(content, differences, urls, filename):
     window = tk.Toplevel()
     window.title("Changed Content")
 
@@ -66,23 +85,30 @@ def show_changed_content(content, differences):
     text_widget.insert(tk.END, content + "\n\nDifferences:\n" + differences)
     text_widget.pack()
 
-    window.mainloop()
+    save_button = tk.Button(window, text="Save Changes", command=lambda: overwrite_file_content(content, filename))
+    save_button.pack()
 
+    window.mainloop()
+    
+    
 def run_gui():
     window = tk.Tk()
     window.title("Webpage Monitor")
     
-    url_label = tk.Label(window, text="URL:")
-    url_label.pack()
-    url_entry = tk.Entry(window)
-    url_entry.pack()
+    url_entries = []
+    for i in range(10):
+        url_label = tk.Label(window, text=f"URL {i+1}:")
+        url_label.pack()
+        url_entry = tk.Entry(window)
+        url_entry.pack()
+        url_entries.append(url_entry)
     
     filename_label = tk.Label(window, text="Filename (without extension):")
     filename_label.pack()
     filename_entry = tk.Entry(window)
     filename_entry.pack()
     
-    submit_button = tk.Button(window, text="Monitor Changes", command=lambda: check_for_changes(url_entry.get(), filename_entry.get()))
+    submit_button = tk.Button(window, text="Monitor Changes", command=lambda: check_for_changes([url_entry.get() for url_entry in url_entries if url_entry.get() != ''], filename_entry.get()))
     submit_button.pack()
     
     window.mainloop()
